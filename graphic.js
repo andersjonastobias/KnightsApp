3f1b19260abd0ab06d39af3419d911ba965026d5
andersjonastobias <andersjonastobias@users.noreply.github.com>
2026-05-17 17:07:06 +0000

    deploy: 9162c43402e1db051ff58feecfee5c09bf21c9b2

---
+++ graphic.js
@@ -0,0 +1,11 @@
+﻿window.graphic = {
+    renderBitmap: function (canvasId, width, height, pixels) {
+        const canvas = document.getElementById(canvasId);
+        const ctx = canvas.getContext("2d");
+        const imageData = new ImageData(
+            new Uint8ClampedArray(pixels),
+            width,
+            height);
+        ctx.putImageData(imageData, 0, 0);
+    }
+};
